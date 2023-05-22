f6c7cd4e3271197459156d41b85be420879b3a43
yzx <103519629+ycbhml@users.noreply.github.com>
2023-05-22 21:14:15 +0900

    解决了窗口之间的传值问题
    暂未解决传进数据库的问题

--- ledger/ledger/Cap_amount.cs
+++ ledger/ledger/Cap_amount.cs
@@ -8,8 +8,12 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using System.Data.SQLite;
+
+
 namespace ledger
 {
+
     public partial class Cap_amount : Form
     {
         public Cap_amount()
@@ -17,11 +21,30 @@ namespace ledger
             InitializeComponent();
         }
 
+        public event Action<string> TextUpdated;
         private void button1_Click(object sender, EventArgs e)  //确定按钮
         {
+            string textboxContent = textBox1.Text; // 获取文本框的内容
+            TextUpdated?.Invoke(textboxContent); // 触发事件，并传递文本框内容
+            this.Close(); // 关闭窗口2
 
+            // 创建一个连接字符串，指定数据库文件路径
+            /*string connectionString = "Data Source=user_info.db;Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)//测试
+                {
+                    Console.WriteLine("数据库已成功打开！");
+                }
+                else
+                {
+                    Console.WriteLine("xxx");
+                }
+                connection.Close();
+            }*/
         }
-
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)//限制textbox不让输入数字
         {
             if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar))
@@ -31,3 +54,6 @@ namespace ledger
         }
     }
 }
+
+
+
--- ledger/ledger/personal ledger.cs
+++ ledger/ledger/personal ledger.cs
@@ -19,33 +19,37 @@ namespace ledger
 
         private void Amount_modification_Click(object sender, EventArgs e)//修改金额的按钮
         {
-            Form Cap_amount = new Cap_amount();// 创建一个新的窗口实例
-
-            // 计算新窗口的位置
-            int x = (Screen.PrimaryScreen.Bounds.Width - Cap_amount.Width) / 2;
-            int y = (Screen.PrimaryScreen.Bounds.Height - Cap_amount.Height) / 2;
+            Cap_amount form2 = new Cap_amount();//建一个新窗口
             // 设置新窗口的位置
-            Cap_amount.StartPosition = FormStartPosition.Manual;
-            Cap_amount.Location = new Point(x, y);
+            int x = (Screen.PrimaryScreen.Bounds.Width - form2.Width) / 2;
+            int y = (Screen.PrimaryScreen.Bounds.Height - form2.Height) / 2;
+            form2.StartPosition = FormStartPosition.Manual;
+            form2.Location = new Point(x, y);
 
-            Cap_amount.Show();// 显示新的窗口
+            //从Cap_amount窗口往personal_ledger窗口传"上限的值"
+            form2.TextUpdated += (content) =>
+            {
+                // 在事件处理程序中更新窗口1的标签
+                shangxian.Text = content;
+            };
+            form2.ShowDialog(); // 显示新的窗口2，并等待其关闭
         }
 
         private void Add_Button_Click(object sender, EventArgs e)//添加条目的按钮
         {
-            Form AddRecord = new AddRecord();// 创建一个新的窗口实例
+             AddRecord Form2 = new AddRecord();// 创建一个新的窗口实例
 
             // 计算新窗口的位置
-            int x = (Screen.PrimaryScreen.Bounds.Width - AddRecord.Width) / 2;
-            int y = (Screen.PrimaryScreen.Bounds.Height - AddRecord.Height) / 2;
+            int x = (Screen.PrimaryScreen.Bounds.Width - Form2.Width) / 2;
+            int y = (Screen.PrimaryScreen.Bounds.Height - Form2.Height) / 2;
             // 设置新窗口的位置
-            AddRecord.StartPosition = FormStartPosition.Manual;
-            AddRecord.Location = new Point(x, y);
+            Form2.StartPosition = FormStartPosition.Manual;
+            Form2.Location = new Point(x, y);
 
-            AddRecord.Show();// 显示新的窗口
+            Form2.Show();// 显示新的窗口
 
             // 在新窗口中查找名为 "tabControl1" 的 TabControl 控件
-            TabControl tabControl = AddRecord.Controls.Find("tabControl1", true).FirstOrDefault() as TabControl;
+            TabControl tabControl = Form2.Controls.Find("tabControl1", true).FirstOrDefault() as TabControl;
             // 如果找到了 TabControl 控件，则选择名为 "tabPage1" 的选项卡
             if (tabControl != null)
             {
