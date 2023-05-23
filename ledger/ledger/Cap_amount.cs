e2efd794353b1204fe001552ea1e7cfb62b5f527
yzx <103519629+ycbhml@users.noreply.github.com>
2023-05-23 17:26:40 +0900

    姚
    姚

--- ledger/ledger/Cap_amount.cs
+++ ledger/ledger/Cap_amount.cs
@@ -20,6 +20,25 @@ namespace ledger
         private void button1_Click(object sender, EventArgs e)  //确定按钮
         {
 
+<<<<<<< Updated upstream
+=======
+            //创建一个连接字符串，指定数据库文件路径
+            string connectionString = "Data Source=user_info.db;Version=3;";
+            using (SQLiteConnection connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                if (connection.State == ConnectionState.Open)//测试
+                {
+                    Console.WriteLine("数据库已成功打开！");
+                }
+                else
+                {
+                    Console.WriteLine("xxx");
+                }
+                connection.Close();
+            }
+>>>>>>> Stashed changes
         }
 
         private void textBox1_KeyPress(object sender, KeyPressEventArgs e)//限制textbox不让输入数字
