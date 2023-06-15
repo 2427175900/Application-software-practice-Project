00e117d3b36ba7a734745e7b5f6438bf20d792f2
2427175900 <a2427175900@gmail.com>
2023-06-16 03:27:52 +0900

    55

--- ledger/ledger/AddUser.cs
+++ ledger/ledger/AddUser.cs
@@ -27,6 +27,7 @@ namespace ledger
         //窗口开始时连接数据库
         private void AddUser_Load(object sender, EventArgs e)
         {
+
            db.dbopen();
         }
 
@@ -42,6 +43,7 @@ namespace ledger
         }
 
         //点击按钮的时候 把 name  插入到数据库
+    
         private void button1_Click(object sender, EventArgs e)
         {
             String name = txtName.Text;
@@ -50,42 +52,38 @@ namespace ledger
             if (maxUser())
             {
                 //接收所有用户名到 names
+
                 string[] names = (db.rtn_name());
 
                 //在没有用户的情况下, 直接进行插入
-                if (names.Length > 0)
-                {
-
-
-                    for (int i = 0; i < names.Length; i++)
-                    {
-                        //用户名是否已存在判断
-                        if (names[i] == name)
-                        {
-                            MessageBox.Show("\"" + name + "\"" + " 사용자 존재합니다. 다시해주세요.");
-                            break;
-                        }
-                        else
-                        {
-
-                            db.insert_new(name);
-                            MessageBox.Show("사용자" + "\"" + name + "\"" + "가 추가완료 되었습니다.");
-                            break;
-
-
-                        }
-                    }
-
-                }
-                else if(name == null)
+                if (name == ""||name ==  null || name == " ")
                 {
                     MessageBox.Show("사용자 이름 입력해주세요");
+                    
                 }
-                else//插入操作完成后 弹窗提示
+                else if (names.Length == 0)
                 {
+
                     db.insert_new(name);
                     MessageBox.Show("사용자" + "\"" + name + "\"" + "가 추가완료 되었습니다.");
+
+
                 }
+                else if (names.Length > 0)
+                {
+                    if (userisnotnull(name))
+                    {
+                        db.insert_new(name);
+                        MessageBox.Show("사용자" + "\"" + name + "\"" + "가 추가완료 되었습니다.");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("사용자" + "\"" + name + "\"" + "가 존재합니다.");
+                    }
+                }
+                
+               
                 //插入完成后关闭该窗口
                 this.Close();
             }
@@ -106,6 +104,22 @@ namespace ledger
                 return false;
             }
             return true;
+        }
+        private bool userisnotnull(string name)
+        {
+            string[] names = (db.rtn_name());
+            for (int j = 0; j < names.Length; j++)
+            {
+                //用户名是否已存在判断
+                if (names[j] == name)
+                {
+                    
+                    return false;
+                }
+                
+            }
+            return true;
+
         }
     }
 }
